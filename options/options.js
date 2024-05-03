602d080ad55cc86cd1fa956491efa3f7c0fb9e17
Crowdbotics <team@crowdbotics.com>
2024-05-03 07:37:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cool-credit-47793",
+  url: "https://cool-credit-47793.botics.co",
+  api: "https://cool-credit-47793.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
